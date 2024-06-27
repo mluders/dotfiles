53ed6b54ea8e9052cab3e53e7951a1868241ba0b
Miles Luders <miles.luders@appfolio.com>
2024-06-27 16:16:46 -0700

    Save current buffer before running tests

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -87,6 +87,7 @@ vim.keymap.set("n", "<leader>t", function()
   local current_file = vim.fn.expand('%')
   local current_line = vim.fn.line('.')
   local command = "terminal test_launcher " .. current_file .. ":" .. current_line
+  vim.cmd('w')
   vim.cmd(command)
 end, { silent = true })
 
