de38078c899a6728598f298a1ed39c1e84114f18
Miles Luders <miles.luders@appfolio.com>
2024-06-28 10:10:09 -0700

    Adjust harpoon width dynamically

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -267,6 +267,12 @@ require("lazy").setup({
   {
     "theprimeagen/harpoon",
     config = function()
+      require("harpoon").setup({
+        menu = {
+          width = vim.api.nvim_win_get_width(0) - 4,
+        }
+      })
+
       local mark = require("harpoon.mark")
       local ui = require("harpoon.ui")
 
