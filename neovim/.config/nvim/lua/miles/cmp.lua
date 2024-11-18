9892c91c6316f725a4500325c2dff5a538c93396
Miles Luders <miles.luders@appfolio.com>
2024-11-18 11:23:16 -0800

    Use arrows keys to navigate completions

--- neovim/.config/nvim/lua/miles/cmp.lua
+++ neovim/.config/nvim/lua/miles/cmp.lua
@@ -1,9 +1,16 @@
-require('cmp').setup({
+local cmp = require('cmp')
+
+cmp.setup({
   sources = {
     { name = 'buffer' },
     { name = 'nvim_lsp' },
     { name = 'path' }
   },
+  mapping = cmp.mapping.preset.insert({
+    ['<Down>'] = cmp.mapping.select_next_item(),
+    ['<Up>'] = cmp.mapping.select_prev_item(),
+    -- ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
 
   -- mapping = cmp.mapping.preset.insert({
   --   -- Enter key confirms completion item
