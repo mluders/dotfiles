62180da1ead0cedc56b4f178a4ea48d5f6566765
Miles Luders <miles.luders@appfolio.com>
2025-01-27 09:41:09 -0800

    Set up nvim-surround

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -34,6 +34,21 @@ require("lazy").setup({
   { "j-hui/fidget.nvim", opts = {} },
   { 'kana/vim-textobj-entire', dependencies = { 'kana/vim-textobj-user' } },
   {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        keymaps = {
+          normal = "gs",
+          normal_cur = "gss",
+          normal_line = "gS",
+          normal_cur_line = "gSS",
+        },
+      })
+    end
+  },
+  {
     'mluders/comfy-line-numbers.nvim',
     opts = {
       up_key = 'l',
