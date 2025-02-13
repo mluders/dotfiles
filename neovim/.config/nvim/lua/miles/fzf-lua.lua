521ce3e46d30b7a42db9c369f576b07e8c85daf2
Miles Luders <miles.luders@appfolio.com>
2025-02-13 15:22:19 -0800

    Improve fzf grep experience

--- neovim/.config/nvim/lua/miles/fzf-lua.lua
+++ neovim/.config/nvim/lua/miles/fzf-lua.lua
@@ -7,7 +7,7 @@ require('fzf-lua').setup({
       ["ctrl-g"] = false -- I never want to search ignored files
     },
     fzf_opts = {
-      ['-i'] = '', -- ignore case
+      ['-i'] = '',             -- ignore case
       ['--layout'] = 'default' -- move search bar to bottom
     },
     git_icons = false
@@ -29,14 +29,14 @@ require('fzf-lua').setup({
 })
 
 vim.keymap.set('n', '<leader>f', require('fzf-lua').files)
-vim.keymap.set('n', '<leader>g', require('fzf-lua').grep)
+vim.keymap.set('n', '<leader>g', require('fzf-lua').live_grep_glob)
 vim.keymap.set('n', '<leader>r', require('fzf-lua').resume)
-vim.keymap.set('n', '<leader>t', function()
-  if list_ft_commands then
-    list_ft_commands()
-  else
-    print("No commands for this file")
-  end
-end)
+-- vim.keymap.set('n', '<leader>t', function()
+--   if list_ft_commands then
+--     list_ft_commands()
+--   else
+--     print("No commands for this file")
+--   end
+-- end)
 -- vim.keymap.set('n', '<leader>;', require('fzf-lua').command_history, { desc = "Fzf Grep" })
 -- vim.keymap.set('n', '<leader>z', ":FzfLua<CR>", { desc = "Fzf Grep" })
